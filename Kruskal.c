db2b9dd04456ce46e644f5069f724899e274b209
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2024-05-31 10:36:54 +0530

    Kruskal ALgo added

---
+++ Kruskal.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define V_MAX 20
+
+struct Edge {
+    int src, dest, weight;
+};
+
+struct Subset {
+    int parent;
+    int rank;
+};
+
+struct Graph {
+    int V, E;
+    struct Edge* edge;
+};
+
+struct Graph* createGraph(int V, int E) {
+    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+    graph->V = V;
+    graph->E = E;
+    graph->edge = (struct Edge*) malloc(graph->E * sizeof(struct Edge));
+    return graph;
+}
+
+int find(struct Subset subsets[], int i) {
+    if (subsets[i].parent != i) {
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    }
+    return subsets[i].parent;
+}
+
+void Union(struct Subset subsets[], int x, int y) {                      // union by rank
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    if (subsets[xroot].rank < subsets[yroot].rank) {
+        subsets[xroot].parent = yroot;
+    } else if (subsets[xroot].rank > subsets[yroot].rank) {
+        subsets[yroot].parent = xroot;
+    } else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+int compareEdges(const void* a, const void* b) {
+    struct Edge* a1 = (struct Edge*)a;
+    struct Edge* b1 = (struct Edge*)b;
+    return a1->weight > b1->weight;
+}
+
+void KruskalMST(struct Graph* graph) {
+    int V = graph->V;
+    struct Edge result[V]; 
+    int e = 0;  
+    int i = 0;  
+
+    qsort(graph->edge, graph->E, sizeof(graph->edge[0]), compareEdges);
+
+    struct Subset* subsets = (struct Subset*) malloc(V * sizeof(struct Subset));
+    for (int v = 0; v < V; ++v) {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    while (e < V - 1 && i < graph->E) {
+        struct Edge next_edge = graph->edge[i++];
+
+        int x = find(subsets, next_edge.src);
+        int y = find(subsets, next_edge.dest);
+
+        if (x != y) {
+            result[e++] = next_edge;
+            Union(subsets, x, y);
+        }
+    }
+
+    printf("The edges in the constructed MST are: \n");
+    for (i = 0; i < e; ++i) {
+        printf("%d -- %d == %d\n", result[i].src, result[i].dest, result[i].weight);
+    }
+
+    free(subsets);
+}
+
+int main() {
+    int V, E;
+    clrscr(); 
+    printf("Enter number of vertices (maximum %d): ", V_MAX);
+    scanf("%d", &V);
+    if (V <= 0 || V > V_MAX) {
+        printf("Invalid input\n");
+        return 1;
+    }
+    
+    printf("Enter number of edges: ");
+    scanf("%d", &E);
+    
+    struct Graph* graph = createGraph(V, E);
+
+    printf("Enter each edge (source, destination, weight):\n");
+    for (int i = 0; i < E; i++) {
+        scanf("%d %d %d", &graph->edge[i].src, &graph->edge[i].dest, &graph->edge[i].weight);
+    }
+
+    KruskalMST(graph);
+    getch(); 
+    return 0;
+}
